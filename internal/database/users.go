ade83fd6bda3d9e873c51e39e5fe6863d8600a92
Sebastian906 <elsebas1912@gmail.com>
2025-06-28 12:06:17 -0500

    Método de autenticación con jsonwebtoken

--- internal/database/users.go
+++ internal/database/users.go
@@ -26,19 +26,27 @@ func (m *UserModel) Insert(user *User) error {
 	return m.DB.QueryRowContext(ctx, query, user.Email, user.Password, user.Name).Scan(&user.Id)
 }
 
+func (m *UserModel) getUser(query string, args ...interface{}) (*User, error) {
+    ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+    defer cancel()
+
+    var user User
+    err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Id, &user.Email, &user.Name, &user.Password)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            return nil, nil
+        }
+        return nil, err
+    }
+    return &user, nil
+}
+
 func (m *UserModel) Get(id int) (*User, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
+    query := `SELECT * FROM users WHERE id = $1`
+    return m.getUser(query, id)
+}
 
-	query := `SELECT * FROM users WHERE id = $1`
-
-	var user User
-	err := m.DB.QueryRowContext(ctx, query, id).Scan(&user.Id, &user.Email, &user.Name, &user.Password)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, nil
-		}
-		return nil, err
-	}
-	return &user, nil
+func (m *UserModel) GetByEmail(email string) (*User, error) {
+    query := `SELECT * FROM users WHERE email = $1`
+    return m.getUser(query, email)
 }
